bd2fbe4ce4b081b03137ced8d49f1157e9e20894
AloizioMacedo <atsam2@hotmail.com>
2023-07-09 19:58:46 -0300

    Change to poise framework
    Possibly bugged.

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,58 @@
-use std::env;
-
-use anyhow::anyhow;
-use serenity::model::gateway::Ready;
-use serenity::model::prelude::{Interaction, InteractionResponseType};
-use serenity::prelude::*;
-use serenity::{async_trait, model::prelude::GuildId};
+use ::serenity::prelude::GatewayIntents;
+use poise::serenity_prelude as serenity;
+use shuttle_poise::ShuttlePoise;
+use shuttle_runtime::Context as _;
 use shuttle_secrets::SecretStore;
-use tracing::info;
 
-struct Bot;
+struct Data {} // User data, which is stored and accessible in all command invocations
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
 
-#[async_trait]
-impl EventHandler for Bot {
-    async fn ready(&self, ctx: Context, ready: Ready) {
-        dotenvy::from_filename("Secrets.toml").unwrap();
+/// Responds with "world!"
+#[poise::command(slash_command)]
+async fn hello(
+    ctx: Context<'_>,
+    #[description = "Your name."] name: Option<String>,
+) -> Result<(), Error> {
+    ctx.say(format!(
+        "Hello, {}!",
+        name.unwrap_or("stranger".to_string())
+    ))
+    .await?;
+    println!("hello");
+    Ok(())
+}
 
-        info!("{} is connected!", ready.user.name);
+#[poise::command(slash_command)]
+async fn heyho(ctx: Context<'_>) -> Result<(), Error> {
+    ctx.say("HEYOH!").await?;
+    println!("heyho");
+    Ok(())
+}
 
-        let guild_id = GuildId(
-            env::var("DISCORD_GUILD_ID")
-                .expect("DISCORD_GUILD_ID env variable should be available.")
-                .parse()
-                .expect("ID should be parseable to u64."),
-        );
+#[shuttle_runtime::main]
+async fn poise(#[shuttle_secrets::Secrets] secret_store: SecretStore) -> ShuttlePoise<Data, Error> {
+    // Get the discord token set in `Secrets.toml`
+    let discord_token = secret_store
+        .get("DISCORD_TOKEN")
+        .context("'DISCORD_TOKEN' was not found")?;
 
-        let commands = GuildId::set_application_commands(&guild_id, &ctx.http, |commands| {
-            commands.create_application_command(|command| {
-                command.name("hello").description("Say hello")
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: vec![hello(), heyho()],
+            ..Default::default()
+        })
+        .token(discord_token)
+        .intents(GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT)
+        .setup(|ctx, _ready, framework| {
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data {})
             })
         })
+        .build()
         .await
-        .unwrap();
+        .map_err(shuttle_runtime::CustomError::new)?;
 
-        info!("{:#?}", commands);
-    }
-
-    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
-        if let Interaction::ApplicationCommand(command) = interaction {
-            let response_content = match command.data.name.as_str() {
-                "hello" => "Hello!".to_owned(),
-                command => unreachable!("Unknown command: {}", command),
-            };
-
-            let create_interaction_response =
-                command.create_interaction_response(&ctx.http, |response| {
-                    response
-                        .kind(InteractionResponseType::ChannelMessageWithSource)
-                        .interaction_response_data(|message| message.content(response_content))
-                });
-
-            if let Err(why) = create_interaction_response.await {
-                eprintln!("Cannot respond to slash command: {}", why);
-            }
-        }
-    }
-}
-#[shuttle_runtime::main]
-async fn serenity(
-    #[shuttle_secrets::Secrets] secret_store: SecretStore,
-) -> shuttle_serenity::ShuttleSerenity {
-    // Get the discord token set in `Secrets.toml`
-    let token = if let Some(token) = secret_store.get("DISCORD_TOKEN") {
-        token
-    } else {
-        return Err(anyhow!("'DISCORD_TOKEN' was not found").into());
-    };
-
-    // Set gateway intents, which decides what events the bot will be notified about
-    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
-
-    let client = Client::builder(&token, intents)
-        .event_handler(Bot)
-        .await
-        .expect("Err creating client");
-
-    Ok(client.into())
+    Ok(framework.into())
 }
